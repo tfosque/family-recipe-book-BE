821bac834c825ae41eaab057845c1ce03bae305d
tfosque@gmail.com <tfosque@gmail.com>
2024-09-25 11:50:45 -0500

    added new edamam be route

---
+++ pages/api/recipes_eda.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { query } = req;
+  const str = query.str;
+
+  // Edamam API credentials
+  const APP_ID = "d9b1cf2a&app"; //process.env.EDAMAM_APP_ID;
+  const APP_KEY = "c5766ec9ad2e1b022e60988c157c48f5"; // process.env.EDAMAM_APP_KEY;
+
+  // Construct the API URL with query parameters
+  const edamamUrl =
+    "https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=chicken&app_id=d9b1cf2a&app_key=c5766ec9ad2e1b022e60988c157c48f5%09";
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&beta=true&q="steak"&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  if (req.method === "GET") {
+    // Handle GET request
+
+    try {
+      // Make the API call
+      const response = await axios.get(url, { params: query });
+      console.log(res.json(response.data));
+      // Send the response back to the client
+      res.status(200).json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch recipes" });
+    }
+
+    res.status(200).json({ message: "Hello from GET request!" });
+  } else if (req.method === "POST") {
+    // Handle POST request
+    const { name } = req.body;
+    res.status(200).json({ message: `Hello ${name} from POST request!` });
+  } else {
+    // Handle any other HTTP method
+    res.setHeader("Allow", ["GET", "POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
