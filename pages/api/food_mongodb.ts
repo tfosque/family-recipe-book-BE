ad3180eef876de0c205269f6d6a3ba10fc44e891
tfosque@gmail.com <tfosque@gmail.com>
2024-09-25 20:30:50 -0500

    moved to api mongo

---
+++ pages/api/food_mongodb.ts
@@ -0,0 +1,22 @@
+import clientPromise from "../../lib/mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const database = "family-book-db";
+  const collection = "food_eda";
+  //
+  try {
+    const client = clientPromise;
+    const db = client.db(database);
+    const food = await db
+      .collection(collection)
+      .find({})
+      .sort({ metacritic: -1 })
+      .limit(27)
+      .toArray();
+    res.json(food);
+    console.log({ food });
+  } catch (e) {
+    console.error(e);
+  }
+};
---
+++ pages/api/recipes_mongodb.ts
@@ -0,0 +1,22 @@
+import clientPromise from "../../lib/mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const database = "family-book-db";
+  const collection = "edamam_recipes";
+  //
+  try {
+    const client = clientPromise;
+    const db = client.db(database);
+    const recipes = await db
+      .collection(collection)
+      .find({})
+      .sort({ metacritic: -1 })
+      .limit(20)
+      .toArray();
+    res.json(recipes);
+    console.log({ recipes });
+  } catch (e) {
+    console.error(e);
+  }
+};
