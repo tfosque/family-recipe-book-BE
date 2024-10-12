c52090f5695e40ef08125e5e93627ee284682319
tfosque@gmail.com <tfosque@gmail.com>
2024-10-12 14:02:02 -0500

    added cors to katy places api route

--- pages/api/katy_places_mongodb.ts
+++ pages/api/katy_places_mongodb.ts
@@ -1,10 +1,13 @@
 import clientPromise from "../../lib/mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
+import cors from "../../lib/app-middleware";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const database = "family-book-db";
   const collection = "katy_places";
   //
+  await cors(req, res);
+
   try {
     const client = clientPromise;
     const db = client.db(database);
